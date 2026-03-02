4a490a5385ef9ae4094d7662dc08715635602063
MohamedAtef2711 <mohamedatefhamza8@gmail.com>
2026-03-03 01:52:16 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,8 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
